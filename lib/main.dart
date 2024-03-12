b6a61b3f05716b416eac3ace5e0fbcf821966665
SergioMallari <156385267+SergioMallari@users.noreply.github.com>
2024-03-12 22:59:25 +0800

    Update main.dart

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:dictionary_api/homepage.dart';
+
+import 'homepage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(),
+      home: HomePage(),
+    );
+  }
+}
